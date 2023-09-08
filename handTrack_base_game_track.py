8884d49fcbd97e19d1df47856812d38d43d7dc16
QaoDePueijo <lucaspinacosta94@gmail.com>
2023-09-08 12:29:26 +0100

    update

--- handTrack_base_game_track.py
+++ handTrack_base_game_track.py
@@ -11,15 +11,30 @@ cTime = 0
 
 cap = cv2.VideoCapture(0) #change camera
 detector = htm.handDetector()
+
+"""
+This code snippet is a while loop that continuously reads frames from a video capture device, detects hands in the frames using a hand detection model, and performs some operations on the detected hands. It also calculates and displays the frames per second (FPS) on the image.
+
+Inputs:
+- cap: A video capture object created using cv2.VideoCapture(0).
+- detector: An instance of the handDetector class from the handTrack module.
+
+Outputs:
+- An annotated image with hand landmarks and FPS displayed.
+"""
+
 while True:
     success, img = cap.read()
+    if not success:
+        break
     img = detector.findHands(img)
-    lmList = detector.findPosition(img,draw=False)
-    if len(lmList) !=0:
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
         print(lmList[4])
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX_SMALL, 1,(255,25,255),1)
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 25, 255), 1)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) == ord('q'):
+        break
